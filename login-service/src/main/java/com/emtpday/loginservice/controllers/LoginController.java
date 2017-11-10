f87f26af1bae603521fc92a49d8d70df8af1696d
Mousa Ibrahim <mousaib85@icloud.com>
2017-11-11 00:43:38 +0200

    fix html files

--- login-service/src/main/java/com/emtpday/loginservice/controllers/LoginController.java
+++ login-service/src/main/java/com/emtpday/loginservice/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
@@ -27,4 +28,5 @@ public class LoginController {
 
         return "LoginSuccess";
     }
+
 }
